bb988afb356a07a5bdb91caf7d67c122ed7f691c
Colin Lancaster <clancaster@west.com>
2016-11-03 14:02:37 -0700

    fixed error

--- main/views.py
+++ main/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {}
-    return(request, 'main/index.html', context)
+    return(request, 'main/index.html', {})
 
